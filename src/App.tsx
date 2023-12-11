8cb3d5d303e253e2e6d3bd92c251f20114106e71
Sonny Mattera <smattera@hotmail.com>
2023-12-11 15:42:59 -0600

    Update App styling and content

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="bg-black">
+    <div className="bg-gray-900">
       <main>
         <div className="relative isolate overflow-hidden">
           <div className="mx-auto max-w-7xl px-6 pb-10 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
@@ -19,14 +19,14 @@ export default function App() {
                   <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20 ring-offset-black">
                     Latest updates
                   </span>
-                  <span className="inline-flex items-center divide-x-[1.5rem] text-sm font-medium leading-6 text-gray-300">
+                  <span className="inline-flex items-center space-x-6 text-sm font-medium leading-6 text-gray-300">
                     <span>Just shipped v1.0</span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
-                      className="h-5 w-5 divide-x-[0.5rem] text-gray-500"
+                      className="h-5 w-5 space-x-2 text-gray-500"
                     >
                       <path
                         fill-rule="evenodd"
@@ -37,7 +37,28 @@ export default function App() {
                   </span>
                 </a>
               </div>
-              <h1>Deploy to the cloud with confidence</h1>
+              <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                Deploy to the cloud with confidence
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+                fugiat aliqua.
+              </p>
+              <div className="mt-10 flex items-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm"
+                >
+                  Get started
+                </a>
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-white"
+                >
+                  Live demo <span aria-hidden="true">→</span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
