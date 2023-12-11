169d74d632ad0f08ef03dcf0bf00eff204fb8f67
Sonny Mattera <smattera@hotmail.com>
2023-12-11 16:22:29 -0600

    Remove react-router-dom dependency and delete unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
