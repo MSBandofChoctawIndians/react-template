89e61f5dd8b6ca4f355f7dc6442ceeb968e6d3d1
Sonny Mattera <smattera@hotmail.com>
2023-12-12 08:21:07 -0600

    Update dependencies: react-router-dom to version 6.20.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
