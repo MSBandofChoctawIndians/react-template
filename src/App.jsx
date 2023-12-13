4ced52c408befbfa0f8e0f8ecb6eb501c5a05b5d
Sonny Mattera <smattera@hotmail.com>
2023-12-13 10:48:01 -0600

    Update vegetable list in App component

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ function App() {
   ]
 
   const vegetables = [
-    { id: 1, name: 'apple', calories: 95 },
-    { id: 2, name: 'orange', calories: 45 },
-    { id: 3, name: 'banana', calories: 105 },
-    { id: 4, name: 'coconut', calories: 159 },
-    { id: 5, name: 'pineapple', calories: 37 }
+    { id: 1, name: 'potatoes', calories: 110 },
+    { id: 2, name: 'celery', calories: 15 },
+    { id: 3, name: 'carrots', calories: 25 },
+    { id: 4, name: 'corn', calories: 63 },
+    { id: 5, name: 'broccoli', calories: 50 }
   ]
 
   return (
@@ -34,6 +34,7 @@ function App() {
       <Student name="Sandy" age={27} isStudent={true} />
       <Student />
       <List items={fruits} category="Fruits" />
+      <List items={vegetables} category="Vegetables" />
     </>
   )
 }
