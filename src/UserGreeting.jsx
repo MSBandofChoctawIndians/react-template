e5a7dc45c45366b32a522972a5fb9d6baa1f7bfe
Sonny Mattera <smattera@hotmail.com>
2023-12-13 09:14:50 -0600

    Update UserGreeting component styling

--- src/UserGreeting.jsx
+++ src/UserGreeting.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function UserGreeting (props) {
-  return (props.isLoggedIn ? <h2>Welcome {props.username}!</h2> : <h2>Please log in to continue</h2>)
+  return (props.isLoggedIn ? <h2 className="welcome-message">Welcome {props.username}!</h2> : <h2 className="login-prompt">Please log in to continue</h2>)
 }
 UserGreeting.propTypes = {
   isLoggedIn: PropTypes.bool,
